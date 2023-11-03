7e19b40863e4e1d592444d0ab0384febdd1edbd3
Abhishek Bose <abhi.bose@couchbase.com>
2023-11-03 10:40:56 +0000

    adds functionality to ignore items other than apples and oranges.

--- src/main/scala/com/scalashopper/v1/ShoppingCart.scala
+++ src/main/scala/com/scalashopper/v1/ShoppingCart.scala
@@ -4,4 +4,7 @@ class ShoppingCart {
   def checkout(basket: List[String]): Double = {
     throw new NotImplementedError("checkout not implemented")
   }
+  def onlyApplesOrOranges(items: List[String]): List[String] = {
+    items.filter(item => item.equalsIgnoreCase("apple") | item.equalsIgnoreCase("orange"))
+  }
 }
--- src/test/scala/com/scalashopper/v1/ShoppingCartSpec.scala
+++ src/test/scala/com/scalashopper/v1/ShoppingCartSpec.scala
@@ -12,4 +12,24 @@ class ShoppingCartSpec extends AnyFlatSpec {
     }
     assert(exception.getMessage.equalsIgnoreCase("checkout not implemented"))
   }
+
+  "onlyApplesOrOranges" should "return empty list when basket is empty" in {
+    val sc = new ShoppingCart()
+    val items = List.empty[String]
+    assert(sc.onlyApplesOrOranges(items) == Nil)
+  }
+
+  "onlyApplesOrOranges" should "return empty list when no matching items found is basket" in {
+    val sc = new ShoppingCart()
+    val items = List("banana", "fig", "grape")
+    assert(sc.onlyApplesOrOranges(items) == Nil)
+  }
+
+  "onlyApplesOrOranges" should "ignore item(s) when basket has items which are not apples or oranges" in {
+    val sc = new ShoppingCart()
+    val items = List("apple", "banana", "apple", "orange", "fig", "grape")
+    assert(sc.onlyApplesOrOranges(items) == List("apple", "apple", "orange"))
+  }
+
+
 }
